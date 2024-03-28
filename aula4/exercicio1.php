684740d7e5bbec8d4cf1f7a4422f2415f340ed7e
Thamiris <thamiris.faria@yahoo.com.br>
2024-03-28 20:40:39 -0300

    exercicio aula4

---
+++ aula4/exercicio1.php
@@ -0,0 +1,10 @@
+<?php
+    function somarNumeros($num1, $num2){
+        $soma = $num1 + $num2;
+        echo "$num1 + $num2 = $soma";
+        echo "<br>"
+    }
+
+    somarNumeros (7, 9);
+
+?>
---
+++ aula4/function.php
@@ -0,0 +1,15 @@
+<?php
+    function Jose() {
+        echo "Ola Paulo! Você é dificil mas eu gosto de você <br>";
+    }
+    
+    Jose();
+
+    function familyName($fname){
+        echo "$fname Siles.<br>";
+    }
+
+    familyName("João");
+    familyName("Lais");
+    familyName("Bug");
+?>
